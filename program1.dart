202211bd9aeddc849f21c360a5bc9ed48f1cbd83
Wlmserick <erickwlms@gmail.com>
2024-03-18 12:58:50 +0300

    Dart hackathon

---
+++ program1.dart
@@ -0,0 +1,8 @@
+void main(){
+    String name ="Erick Ambwara";
+    int age = 24;
+    String school = "PLP Academy";
+    String hobby = "Coding";
+
+    print("My name is $name. I am $age years old and I attend $school. My hobby is $hobby.");
+}

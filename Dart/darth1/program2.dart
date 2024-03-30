9da0d81e8b854f0796642e82321834e2ae2b14ee
Wlmserick <erickwlms@gmail.com>
2024-03-30 13:14:05 +0300

    flow control

---
+++ Dart/darth1/program2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int num1 = 905;
+  int num2 = 56;
+  
+  print("Sum: ${sum(num1, num2)}");
+  print("Difference: ${difference(num1, num2)}");
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int difference(int a, int b) {
+  return a - b;
+}
